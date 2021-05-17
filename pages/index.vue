4138db793f8029662f6ae425cde8d619ed91ccad
Yen-Ju-Chen <61488935+Yen-Ju-Chen@users.noreply.github.com>
2021-05-17 14:49:58 +0800

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {};
+</script>
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
